f8feacc2f323a7d8bf771b89cf55e3ce1191556e
DoJale007 <israeljale98@gmail.com>
2025-09-16 10:45:28 +0000

    Including the favorites and get favorites endpoints

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, HTTPException
 import requests
+from pydantic import BaseModel
+from util import replace_mongo_id
 from db import favorites_collection
 
-
 app = FastAPI()
+
+
+class CountryModel(BaseModel):
+    name: str
+    user_notes: str
 
 
 @app.get("/")
@@ -18,7 +24,7 @@ def get_country_by_name(name: str):
     if response.status_code != 200:
         raise HTTPException(status_code=404, detail="Country not found")
 
-    data = response.json()[0]  # take the first match
+    data = response.json()[0]  # returns the first match from the ext. api
     country_info = {
         "name": data["name"]["common"],
         "capital": data.get("capital", ["N/A"])[0],
@@ -26,3 +32,35 @@ def get_country_by_name(name: str):
         "region": data.get("region", "N/A"),
     }
     return country_info
+
+
+@app.post("/countries/favorites")
+def save_favorite_country(country: CountryModel):
+    country_link = f"https://restcountries.com/v3.1/name/{country.name}"
+    response = requests.get(country_link)
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail="Country not found")
+    data = response.json()[0]
+    country_info = {
+        "name": data["name"]["common"],
+        "capital": data.get("capital", ["N/A"])[0],
+        "population": data.get("population", "N/A"),
+        "region": data.get("region", "N/A"),
+        "user_notes": country.user_notes,
+    }
+
+    existing_country = favorites_collection.find_one({"name": country_info["name"]})
+    if existing_country:
+        raise HTTPException(
+            status_code=409, detail=f"{country_info["name"]} is already a favorite"
+        )
+
+    favorites_collection.insert_one(country_info)
+    return {"Message": f"{country_info["name"]} saved to favorites!"}
+
+
+@app.get("/favorites")
+def get_all_favorites():
+    favorite_countries = favorites_collection.find()
+    favorites_list = list(favorite_countries)
+    return {"data": list(map(replace_mongo_id, favorites_list))}
